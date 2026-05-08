085a8ff7f4130dde4b931f5c6f9b37f4325b0cb7
envision19 <envision.trading10@gmail.com>
2026-05-09 00:27:43 +0800

    Update vite config to optimize chunks and remove warnings

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // This increases the limit to 1000kb to stop the warning
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        // This splits your libraries (like React) into a separate file 
+        // so your main code stays small and fast
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        },
+      },
+    },
+  },
 })
